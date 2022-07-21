764393f6af6aa9dd8c0b3c88a4eb00ed428a464a
MoonGyu1 <s201801402@hufs-gsuite.kr>
2022-07-21 14:25:42 +0800

    add: change mysql2 to mysql2/promise

--- src/repository/db.js
+++ src/repository/db.js
@@ -1,25 +1,14 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const dbConfig = require('./dbConfig');
+const dbConfig = require('../config/dbConfig');
 
 console.log(`[🔥DB] ${process.env.NODE_ENV}`);
 
 // connection pool 생성
-const pool = mysql.createPool({
+module.exports = mysql.createPool({
   ...dbConfig,
   connectionLimit: 10,
   waitForConnections: true, // 사용가능한 pool이 없을 경우 대기
-  acquireTimeout: 60 * 1000,
 });
-
-function connect(callback) {
-  pool.getConnection(function (err, conn) {
-    if (!err) {
-      callback(conn);
-    }
-  });
-}
-
-module.exports = { connect };
