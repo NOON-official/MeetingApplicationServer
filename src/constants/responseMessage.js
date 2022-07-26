8e1462268a7602c25f2bb2ac3f41c6516dd33186
MoonGyu1 <s201801402@hufs-gsuite.kr>
2022-07-26 17:37:04 +0800

    add: 미팅학개론 response message 추가

--- src/constants/responseMessage.js
+++ src/constants/responseMessage.js
@@ -3,29 +3,15 @@ module.exports = {
   TOKEN_EMPTY: '토큰이 없습니다',
   TOKEN_INVALID: '토큰이 유효하지 않습니다',
 
-  NULL_VALUE: '필요한 값이 없습니다',
+  NULL_VALUE: '필수 항목을 모두 입력해주세요',
   OUT_OF_VALUE: '파라미터 값이 잘못되었습니다',
 
   INTERNAL_SERVER_ERROR: '서버 내 오류',
-  FILE_TYPE_ERROR: '잘못된 파일 형식입니다',
 
-  // 회원가입
-  CREATE_USER: '회원 가입에 성공하였습니다',
-  BLANK_BOX: '정보가 올바르지 않습니다. 다시 입력해주세요.',
-  ALREADY_EMAIL: '이미 존재하는 이메일입니다.',
+  // 미팅학개론
+  GET_WEEKLY_COUNT_SUCCESS: '주간 매칭 건수 조회에 성공하였습니다',
 
-  // 로그인
-  LOGIN_SUCCESS: '로그인에 성공하였습니다',
-  LOGIN_FAIL: '로그인에 실패하였습니다',
-  NO_USER: '존재하지 않는 회원입니다',
-  MISS_MATCH_PW: '비밀번호가 맞지 않습니다',
-
-  // 투두리스트 카드
-  CARD_BLANK: '필수 항목을 모두 입력해주세요',
-  CREATE_CARD_SUCCESS: '카드 생성에 성공하였습니다',
-  GET_CARD_SUCCESS: '모든 카드 조회에 성공하였습니다',
-  NO_CARD: '존재하지 않는 카드입니다',
-  UPDATE_CARD_SUCCESS: '카드 상태 업데이트에 성공하였습니다',
-  INVALID_CARD_STATE: '유효하지 않은 카드 상태입니다',
-  DELETE_CARD_SUCCESS: '카드 삭제에 성공하였습니다',
+  // 유저
+  SAVE_USER_SUCCESS: '유저 정보 저장에 성공하였습니다',
+  SAVE_USER_PREFERENCE_SUCCESS: '유저 선호 정보 저장에 성공하였습니다',
 };
