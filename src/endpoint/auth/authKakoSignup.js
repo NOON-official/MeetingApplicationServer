b38cdd6ed0e7c6a4510293d37f4c7f54d90b9d70
MoonGyu1 <s201801402@hufs-gsuite.kr>
2022-08-15 22:06:39 +0800

    add: 카카오 로그인

---
+++ src/endpoint/auth/authKakoSignup.js
@@ -0,0 +1,40 @@
+const pool = require('../../repository/db');
+const { userDB } = require('../../repository');
+const jwt = require('../../lib/jwtHandlers');
+
+module.exports = async (kakaoUser) => {
+  const { id: kakaoUid, properties, kakao_account: kakaoAccount } = kakaoUser;
+
+  if (!kakaoUid || !properties.nickname) {
+    return false;
+  }
+
+  let conn;
+
+  try {
+    conn = await pool.getConnection();
+
+    // DB에 유저 생성
+    let user = await userDB.saveUser(conn, kakaoUid, properties, kakaoAccount);
+
+    // JWT 발급
+    const accessToken = jwt.accessToken.sign(user);
+    const refreshToken = jwt.refreshToken.sign();
+
+    // 발급한 Refresh Token을 DB에 저장
+    await userDB.saveRefreshToken(conn, refreshToken, user.id);
+
+    user.refreshToken = refreshToken;
+    user.accessToken = accessToken;
+
+    // 토큰과 함께 유저 정보를 반환
+    return user;
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(statusCode.INTERNAL_SERVER_ERROR)
+      .send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+  } finally {
+    conn.release(); // 빌려온 connection 반환
+  }
+};
