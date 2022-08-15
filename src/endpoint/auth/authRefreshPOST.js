fd84f4b5ac778f120096cbd02f226035cb38d76e
MoonGyu1 <s201801402@hufs-gsuite.kr>
2022-08-15 22:08:04 +0800

    add: JWT (Access Token, Refresh Token)

---
+++ src/endpoint/auth/authRefreshPOST.js
@@ -0,0 +1,84 @@
+const jwtHandlers = require('../../lib/jwtHandlers');
+const jwt = require('jsonwebtoken');
+const pool = require('../../repository/db');
+const { userDB } = require('../../repository');
+const util = require('../../lib/util');
+const statusCode = require('../../constants/statusCode');
+const responseMessage = require('../../constants/responseMessage');
+const { TOKEN_INVALID, TOKEN_EXPIRED } = require('../../constants/jwt');
+
+module.exports = async (req, res) => {
+  // Access Token과 Refresh Token 추출
+  const accessToken = req.headers.authorization.split('Bearer ')[1];
+  const grantType = req.body.grant_type;
+  const refreshToken = req.body.refresh_token;
+  const userId = req.body.userId;
+
+  if (!accessToken || !refreshToken) {
+    return res.status(statusCode.UNAUTHORIZED).send(util.fail(statusCode.UNAUTHORIZED, responseMessage.TOKEN_EMPTY));
+  }
+
+  if (grantType != 'refresh_token') {
+    return res
+      .status(statusCode.UNAUTHORIZED)
+      .send(util.fail(statusCode.UNAUTHORIZED, responseMessage.INVALID_GRANT_TYPE));
+  }
+
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    const decoded = jwt.decode(accessToken);
+
+    // 디코딩 결과가 없는 경우
+    if (decoded === null) {
+      return res
+        .status(statusCode.UNAUTHORIZED)
+        .send(util.fail(statusCode.UNAUTHORIZED, responseMessage.TOKEN_INVALID));
+    }
+
+    // Access Token 해독 및 인증
+    const decodedAccessToken = jwtHandlers.accessToken.verify(accessToken);
+    const decodedRefreshToken = await jwtHandlers.refreshToken.verify(refreshToken, userId);
+
+    // 토큰이 유효하지 않은 경우
+    if (decodedAccessToken === TOKEN_INVALID || decodedRefreshToken === TOKEN_INVALID) {
+      return res
+        .status(statusCode.UNAUTHORIZED)
+        .send(util.fail(statusCode.UNAUTHORIZED, responseMessage.TOKEN_INVALID));
+    }
+
+    // 토큰 재발급 과정
+    if (decodedAccessToken === TOKEN_EXPIRED) {
+      // 1. Access Token 만료 & Refresh Token 만료 => 새로 로그인
+      if (decodedRefreshToken === TOKEN_EXPIRED) {
+        return res
+          .status(statusCode.UNAUTHORIZED)
+          .send(util.fail(statusCode.UNAUTHORIZED, responseMessage.TOKEN_EXPIRED));
+      }
+      // 2. Access Token 만료 & Refresh Token 유효 => Access Token 재발급
+      else {
+        let user = userDB.getUserById(conn, decoded.id);
+        const newAccessToken = jwtHandlers.accessToken.sign(user);
+
+        user.accessToken = newAccessToken;
+        user.refreshToken = refreshToken;
+
+        // 새로 발급한 Access Token과 함께 유저 정보를 반환
+        return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.REFRESH_TOKEN_SUCCESS, user));
+      }
+    }
+    // 3. Access Token 유효 => 재발급 필요X
+    else {
+      return res
+        .status(statusCode.BAD_REQUEST)
+        .send(util.fail(statusCode.BAD_REQUEST, responseMessage.NOT_EXPIRED_TOKEN));
+    }
+  } catch (error) {
+    console.log(error);
+    res
+      .status(statusCode.INTERNAL_SERVER_ERROR)
+      .send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+  } finally {
+    conn.release();
+  }
+};
---
+++ src/lib/jwtHandlers.js
@@ -0,0 +1,106 @@
+const jwt = require('jsonwebtoken');
+const pool = require('../repository/db');
+const { userDB } = require('../repository');
+const { TOKEN_INVALID, TOKEN_EXPIRED } = require('../constants/jwt');
+
+// JWT 옵션 설정
+const secretKey = process.env.JWT_SECRET;
+const accessTokenOptions = {
+  algorithm: 'HS256',
+  expiresIn: '1h', // Access Token 기한
+  issuer: 'meetinghakgaeron',
+};
+
+const accessToken = {
+  // Access Token 발급
+  sign(user) {
+    const payload = {
+      id: user.id,
+      kakaoUid: user.kakaoUid,
+      name: user.nickname,
+    };
+
+    const result = jwt.sign(payload, secretKey, accessTokenOptions);
+
+    return result;
+  },
+
+  // Access Token 검증
+  verify(token) {
+    let decoded;
+    try {
+      decoded = jwt.verify(token, secretKey);
+    } catch (err) {
+      if (err.message === 'jwt expired') {
+        console.log('expired access token');
+        return TOKEN_EXPIRED;
+      } else if (err.message === 'invalid token') {
+        console.log('invalid access token 1');
+        return TOKEN_INVALID;
+      } else {
+        console.log('invalid access token 2');
+        return TOKEN_INVALID;
+      }
+    }
+    // 해독된 상태의 JWT 반환
+    return decoded;
+  },
+};
+
+const refreshTokenOptions = {
+  algorithm: 'HS256',
+  expiresIn: '14d', // Refresh Token 기한
+  issuer: 'meetinghakgaeron',
+};
+
+const refreshToken = {
+  // Refresh Token 발급
+  sign() {
+    const result = jwt.sign({}, secretKey, refreshTokenOptions); // payload 없음
+
+    return result;
+  },
+
+  // Refresh Token 검증
+  async verify(token, userId) {
+    let conn;
+    let decoded;
+
+    try {
+      conn = await pool.getConnection();
+      const dbToken = await userDB.getRefreshTokenByUserId(conn, userId);
+
+      if (!dbToken) {
+        console.log('invalid user');
+        return TOKEN_INVALID;
+      }
+
+      // 클라이언트에서 보낸 토큰과 DB에 저장된 토큰이 같으면
+      if (token === dbToken) {
+        decoded = jwt.verify(token, secretKey);
+      } else {
+        return TOKEN_INVALID;
+      }
+    } catch (err) {
+      if (err.message === 'jwt expired') {
+        console.log('expired refresh token');
+        return TOKEN_EXPIRED;
+      } else if (err.message === 'invalid token') {
+        console.log('invalid refresh token 1');
+        return TOKEN_INVALID;
+      } else {
+        console.log('invalid refresh token 2');
+        return TOKEN_INVALID;
+      }
+    } finally {
+      conn.release();
+    }
+
+    return decoded;
+  },
+};
+
+module.exports = {
+  accessToken,
+  refreshToken,
+};
