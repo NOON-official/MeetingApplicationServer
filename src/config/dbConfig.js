8a55febb3001902b90def6045307602f2a125446
MoonGyu1 <s201801402@hufs-gsuite.kr>
2022-08-22 16:32:48 +0800

    add: local db config

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,6 +1,15 @@
-module.exports = {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  };
+} else {
+  module.exports = {
+    host: process.env.LOCAL_DB_HOST,
+    user: process.env.LOCAL_DB_USER,
+    password: process.env.LOCAL_DB_PASSWORD,
+    database: process.env.LOCAL_DB_DATABASE,
+  };
+}
