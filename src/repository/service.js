ac597e37edec5806d3b290c6f9a1de5ba04e2894
MoonGyu1 <s201801402@hufs-gsuite.kr>
2022-08-30 16:34:12 +0900

    fix: 실제 신청중인 유저 수 조회

--- src/repository/service.js
+++ src/repository/service.js
@@ -2,8 +2,10 @@ const convertSnakeToCamel = require('../lib/convertSnakeToCamel');
 
 const getWaitingTeam = async (conn) => {
   // 'mysql2/promise'는 결과값을 배열로 반환
-  const [row] = await conn.query('SELECT waiting_team FROM service;');
+  const [row] = await conn.query('SELECT COUNT(*) AS waiting_team FROM `user_ourteam` WHERE is_deleted=false;');
   result = row[0]['waiting_team'];
+
+  if (!result) result = 0;
 
   return convertSnakeToCamel.keysToCamel(result);
 };
