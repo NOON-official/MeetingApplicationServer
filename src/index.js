3d0b5ebf4b954a537c59a9989326afb6b52a6475
MoonGyu1 <s201801402@hufs-gsuite.kr>
2022-08-31 17:14:25 +0900

    fix: cross env 문법오류 해결

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
+const path = require('path');
 const dotenv = require('dotenv');
-dotenv.config();
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 const { createApp } = require('./app');
 const urlConfig = require('./config/urlConfig');
