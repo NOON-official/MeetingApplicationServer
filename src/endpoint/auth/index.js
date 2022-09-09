d2bdb381a4c5553089ea125d22369ce8b600c2e2
MoonGyu1 <s201801402@hufs-gsuite.kr>
2022-09-10 00:28:26 +0900

    mv: to user directory

--- src/endpoint/auth/index.js
+++ src/endpoint/auth/index.js
@@ -5,6 +5,5 @@ const { checkUser } = require('../../middlewares/auth');
 router.get('/kakao/callback', require('./authKakaoCallbackGET'));
 router.delete('/account/:userId', checkUser, require('./authAccountUserIdDELETE'));
 router.post('/refresh', require('./authRefreshPOST'));
-router.post('/phone', checkUser, require('./authPhonePOST'));
 
 module.exports = router;
