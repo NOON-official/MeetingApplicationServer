d09002c4ab463591b74f78e8e37568d01cd249d8
MoonGyu1 <s201801402@hufs-gsuite.kr>
2022-09-11 00:31:26 +0900

    feat: 우리팀의 상대팀 아이디 조회

--- src/endpoint/team/index.js
+++ src/endpoint/team/index.js
@@ -5,6 +5,7 @@ const { checkUser } = require('../../middlewares/auth');
 router.post('/', checkUser, require('./teamPOST'));
 router.put('/', checkUser, require('./teamPUT'));
 router.get('/ourteam-id/:userId', checkUser, require('./teamOurteamIdUserIdGET'));
+router.get('/partner-team-id/:ourteamId', checkUser, require('./teamPartnerTeamIdOurteamIdGET.js'));
 router.get('/:teamId', checkUser, require('./teamTeamIdGET.js'));
 
 module.exports = router;
