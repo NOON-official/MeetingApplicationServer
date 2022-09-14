0bffb209a2a743f279e4f9ba599b05a5fbc1e32a
MoonGyu1 <s201801402@hufs-gsuite.kr>
2022-09-14 21:49:59 +0900

    feat: 관리자 페이지 매칭 완료된 팀 정보 조회

--- src/endpoint/admin/index.js
+++ src/endpoint/admin/index.js
@@ -7,5 +7,7 @@ router.get('/team/male/2', checkUser, checkAdmin, require('./adminTeamMale2GET')
 router.get('/team/male/3', checkUser, checkAdmin, require('./adminTeamMale3GET'));
 router.get('/team/female/2', checkUser, checkAdmin, require('./adminTeamFemale2GET'));
 router.get('/team/female/3', checkUser, checkAdmin, require('./adminTeamFemale3GET'));
+router.get('/team/matching/success/male', checkUser, checkAdmin, require('./adminTeamMatchingSuccessMale'));
+router.get('/team/matching/success/female', checkUser, checkAdmin, require('./adminTeamMatchingSuccessFemale'));
 
 module.exports = router;
