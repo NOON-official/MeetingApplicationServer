6cb3787927bfb73954a50d353f14119907b25c5f
MoonGyu1 <s201801402@hufs-gsuite.kr>
2022-10-15 15:18:06 +0900

    fix: add http method to file name

--- src/endpoint/admin/index.js
+++ src/endpoint/admin/index.js
@@ -7,15 +7,15 @@ router.get('/team/male/2', checkUser, checkAdmin, require('./adminTeamMale2GET')
 router.get('/team/male/3', checkUser, checkAdmin, require('./adminTeamMale3GET'));
 router.get('/team/female/2', checkUser, checkAdmin, require('./adminTeamFemale2GET'));
 router.get('/team/female/3', checkUser, checkAdmin, require('./adminTeamFemale3GET'));
-router.get('/team/matching/success/male', checkUser, checkAdmin, require('./adminTeamMatchingSuccessMale'));
-router.get('/team/matching/success/female', checkUser, checkAdmin, require('./adminTeamMatchingSuccessFemale'));
-router.get('/team/matching/fail/male', checkUser, checkAdmin, require('./adminTeamMatchingFailMale'));
-router.get('/team/matching/fail/female', checkUser, checkAdmin, require('./adminTeamMatchingFailFemale'));
+router.get('/team/matching/success/male', checkUser, checkAdmin, require('./adminTeamMatchingSuccessMaleGET'));
+router.get('/team/matching/success/female', checkUser, checkAdmin, require('./adminTeamMatchingSuccessFemaleGET'));
+router.get('/team/matching/fail/male', checkUser, checkAdmin, require('./adminTeamMatchingFailMaleGET'));
+router.get('/team/matching/fail/female', checkUser, checkAdmin, require('./adminTeamMatchingFailFemaleGET'));
 router.post('/team/match', checkUser, checkAdmin, require('./adminTeamMatchPOST'));
 router.post('/close/matching', checkUser, checkAdmin, require('./adminCloseMatchingPOST'));
 router.post('/close/team', checkUser, checkAdmin, require('./adminCloseTeamPOST'));
 router.put('/apply/status', checkUser, checkAdmin, require('./adminApplyStatusPUT'));
-router.post('/team/fail', checkUser, checkAdmin, require('./adminTeamFail'));
+router.post('/team/fail', checkUser, checkAdmin, require('./adminTeamFailPOST'));
 router.get('/apply/status', checkUser, checkAdmin, require('./adminApplyStatusGET'));
 router.put('/team/match', checkUser, checkAdmin, require('./adminTeamMatchPUT'));
 
