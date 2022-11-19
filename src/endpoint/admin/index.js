907b588c8bb96277b0eab029f8484cc6776052c9
MoonGyu1 <s201801402@hufs-gsuite.kr>
2022-11-19 18:21:53 +0900

    feat: 남자팀 2:2 매칭 신청 정보 전체 조회 (가신청)

--- src/endpoint/admin/index.js
+++ src/endpoint/admin/index.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const { checkUser } = require('../../middlewares/auth');
 const { checkAdmin } = require('../../middlewares/admin');
 
-router.get('/team/male/2', checkUser, checkAdmin, require('./adminTeamMale2GET'));
-router.get('/team/male/3', checkUser, checkAdmin, require('./adminTeamMale3GET'));
-router.get('/team/female/2', checkUser, checkAdmin, require('./adminTeamFemale2GET'));
-router.get('/team/female/3', checkUser, checkAdmin, require('./adminTeamFemale3GET'));
+router.get('/team/male/2/unchecked', checkUser, checkAdmin, require('./adminTeamMale2UncheckedGET'));
+router.get('/team/male/3/unchecked', checkUser, checkAdmin, require('./adminTeamMale3UncheckedGET'));
+router.get('/team/female/2/unchecked', checkUser, checkAdmin, require('./adminTeamFemale2UncheckedGET'));
+router.get('/team/female/3/unchecked', checkUser, checkAdmin, require('./adminTeamFemale3UncheckedGET'));
 router.get('/team/matching/success/male', checkUser, checkAdmin, require('./adminTeamMatchingSuccessMaleGET'));
 router.get('/team/matching/success/female', checkUser, checkAdmin, require('./adminTeamMatchingSuccessFemaleGET'));
 router.get('/team/matching/fail/male', checkUser, checkAdmin, require('./adminTeamMatchingFailMaleGET'));
