dfd3671d5b14d7790b39c6187a0b5459c9ad826e
MoonGyu1 <s201801402@hufs-gsuite.kr>
2023-01-22 17:59:35 +0900

    docs: add api property options

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -3,37 +3,59 @@ import { IsNotEmpty, IsString, IsOptional, IsNumber } from 'class-validator';
 
 export class CreateUserDto {
   @ApiProperty({
-    example: 1355387,
+    description: '카카오 유저아이디',
+    example: 2385693417,
     required: true,
   })
   @IsNotEmpty()
   @IsNumber()
   readonly kakaoUid: number;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: '카카오 닉네임',
+    example: '미팅이',
+    required: true,
+  })
   @IsNotEmpty()
   @IsString()
   readonly nickname: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: '핸드폰 번호',
+    example: '01012345678',
+  })
   @IsOptional()
   @IsString()
   readonly phone?: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: '카카오 성별',
+    example: 'male',
+  })
   @IsString()
   readonly gender?: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: '카카오 연령대',
+    example: '20-29',
+  })
   @IsString()
   readonly ageRange?: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: '추천인 코드',
+    example: 'LD4GSTO3',
+    required: true,
+  })
   @IsNotEmpty()
   @IsString()
   readonly referralId: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: '리프레시 토큰',
+    example:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoi66y46rec7JuQIiwic3ViIjoxNDcsImlhdCI6MTY3NDAzMzczNywiZXhwIjoxNjc0MDQwOTM3fQ.Xvzy6JGnMK_av67jTuuQeRuPp7TZkExGgwnUnIWezpE',
+  })
   @IsOptional()
   @IsString()
   readonly refreshToken?: string;
