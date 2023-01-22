9d27e3387bd90810fee5aa15e359d4c976a36348
MoonGyu1 <s201801402@hufs-gsuite.kr>
2023-01-22 23:19:48 +0900

    add: create team dto

---
+++ src/teams/dtos/create-team.dto.ts
@@ -0,0 +1,204 @@
+import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsNotEmpty,
+  IsString,
+  IsInt,
+  IsArray,
+  IsOptional,
+  ValidateNested,
+  IsDate,
+  ArrayMinSize,
+  ArrayMaxSize,
+  Min,
+  Max,
+} from 'class-validator';
+
+export class createMemberDto {
+  @ApiProperty({
+    description: '포지션',
+    example: 1,
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsInt()
+  @Min(1)
+  @Max(4)
+  readonly role: number;
+
+  @ApiProperty({
+    description: 'MBTI',
+    example: 13,
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsInt()
+  @Min(1)
+  @Max(16)
+  readonly mbti: number;
+
+  @ApiProperty({
+    description: '닮은꼴',
+    example: '차은우',
+  })
+  @IsOptional()
+  @IsString()
+  readonly appearance?: string;
+
+  @ApiProperty({
+    description: '나이',
+    example: 23,
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsInt()
+  @Min(19)
+  @Max(20)
+  readonly age: number;
+}
+
+export class CreateTeamDto {
+  @ApiProperty({
+    description: '팀 생성하는 유저아이디',
+    example: 1,
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsInt()
+  readonly ownerId: number;
+
+  @ApiProperty({
+    description: '성별',
+    example: 1,
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsInt()
+  @Min(1)
+  @Max(2)
+  readonly gender: number;
+
+  @ApiProperty({
+    description: '인원수',
+    example: 2,
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsInt()
+  @Min(2)
+  @Max(3)
+  readonly memberCount: number;
+
+  @ApiProperty({
+    description: '대학교',
+    example: [1, 42, 345],
+    required: true,
+  })
+  @IsInt({ each: true })
+  @Min(1, { each: true })
+  @Max(4, { each: true })
+  @ArrayMinSize(0)
+  @ArrayMaxSize(3)
+  readonly university?: number[];
+
+  @ApiProperty({
+    description: '미팅 선호 날짜',
+    example: ['2023-01-22', '2023-01-23', '2023-01-24'],
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsDate({ each: true })
+  @ArrayMinSize(4)
+  readonly availableDate: Date[];
+
+  @ApiProperty({
+    description: '지역',
+    example: [1, 3],
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsInt({ each: true })
+  @Min(1, { each: true })
+  @Max(5, { each: true })
+  @ArrayMinSize(1)
+  @ArrayMaxSize(5)
+  readonly area: number[];
+
+  @ApiProperty({
+    type: [createMemberDto],
+    description: '멤버',
+    required: true,
+  })
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => createMemberDto)
+  @ArrayMinSize(2)
+  @ArrayMaxSize(3)
+  readonly members: createMemberDto[];
+
+  @ApiProperty({
+    description: '한 줄 소개',
+    example: '안녕하세요',
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsString()
+  readonly intro: string;
+
+  @ApiProperty({
+    description: '주량 레벨',
+    example: 5,
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsInt()
+  @Min(1)
+  @Max(5)
+  readonly drink: number;
+
+  @ApiProperty({
+    description: '상대방 학교',
+    example: 1,
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsInt()
+  @Min(1)
+  @Max(2)
+  readonly prefSameUniversity: 1 | 2;
+
+  @ApiProperty({
+    description: '상대방 선호 나이 최솟값',
+    example: 23,
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsInt()
+  @Min(19)
+  @Max(29)
+  readonly prefMinAge: number;
+
+  @ApiProperty({
+    description: '상대방 선호 나이 최댓값',
+    example: 27,
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsInt()
+  @Min(19)
+  @Max(29)
+  readonly prefMaxAge: number;
+
+  @ApiProperty({
+    description: '분위기',
+    example: [1, 2, 5],
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsInt({ each: true })
+  @Min(1, { each: true })
+  @Max(5, { each: true })
+  @ArrayMinSize(1)
+  @ArrayMaxSize(5)
+  readonly prefVibe: number[];
+}
