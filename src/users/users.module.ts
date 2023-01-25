37552f8928fdf5ba91423ab8598179ac45b37418
MoonGyu1 <s201801402@hufs-gsuite.kr>
2023-01-25 18:37:53 +0900

    feat: 친구 초대 횟수 조회

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,16 @@
+import { InvitationsModule } from './../invitations/invitations.module';
 import { UsersRepository } from './repositories/users.repository';
 import { UserAgreementsRepository } from './repositories/user-agreements.repository';
 import { TypeOrmExModule } from '../database/typeorm-ex.module';
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [TypeOrmExModule.forCustomRepository([UsersRepository, UserAgreementsRepository])],
+  imports: [
+    TypeOrmExModule.forCustomRepository([UsersRepository, UserAgreementsRepository]),
+    forwardRef(() => InvitationsModule),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
