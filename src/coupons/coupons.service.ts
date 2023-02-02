7f5457384427551f3912a639e99189ea144c3e6e
MoonGyu1 <s201801402@hufs-gsuite.kr>
2023-02-02 22:10:51 +0900

    feat: 쿠폰 등록

--- src/coupons/coupons.service.ts
+++ src/coupons/coupons.service.ts
@@ -1,12 +1,20 @@
+import { BadRequestException } from '@nestjs/common/exceptions';
 import { UserCoupon } from './../users/interfaces/user-coupon.interface';
 import { CouponsRepository } from './repositories/coupons.repository';
-import { Injectable } from '@nestjs/common';
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { Coupon } from './entities/coupon.entity';
 import { Coupons, CouponType } from './constants/coupons';
+import { RegisterCouponDto } from './dtos/register-coupon.dto';
+import * as moment from 'moment-timezone';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class CouponsService {
-  constructor(private couponsRepository: CouponsRepository) {}
+  constructor(
+    private couponsRepository: CouponsRepository,
+    @Inject(forwardRef(() => UsersService))
+    private usersService: UsersService,
+  ) {}
 
   async getCouponById(couponId: number): Promise<Coupon> {
     return this.couponsRepository.getCouponById(couponId);
@@ -26,5 +34,31 @@ export class CouponsService {
 
   async getCouponsPagedata(): Promise<{ Coupons: CouponType[] }> {
     return { Coupons };
+  }
+
+  async registerCoupon(userId: number, registerCouponDto: RegisterCouponDto): Promise<void> {
+    const coupon = await this.couponsRepository.getCouponByCode(registerCouponDto.code);
+
+    // 이미 등록된 쿠폰인 경우
+    if (!!coupon.user) {
+      throw new BadRequestException('already registered coupon');
+    }
+
+    // 이미 사용한 쿠폰인 경우
+    if (!!coupon.usedAt) {
+      throw new BadRequestException('already used coupon');
+    }
+
+    // 기한이 만료된 쿠폰인 경우
+    if (!!coupon.expiresAt) {
+      const today = new Date(moment().tz('Asia/Seoul').format('YYYY-MM-DD'));
+      const expiresDay = new Date(coupon.expiresAt);
+
+      if (expiresDay < today) {
+        throw new BadRequestException('already expired coupon');
+      }
+    }
+
+    return this.couponsRepository.registerCoupon(coupon.id, userId);
   }
 }
--- src/coupons/dtos/register-coupon.dto.ts
+++ src/coupons/dtos/register-coupon.dto.ts
@@ -1,16 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString, IsInt } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class RegisterCouponDto {
-  @ApiProperty({
-    description: '유저ID',
-    example: 1,
-    required: true,
-  })
-  @IsNotEmpty()
-  @IsInt()
-  readonly userId: number;
-
   @ApiProperty({
     description: '쿠폰 코드',
     example: 'ABCD1234',
