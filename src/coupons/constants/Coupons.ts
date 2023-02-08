f246a62e8247a8919058eaec2649aec4a56d0c48
MoonGyu1 <s201801402@hufs-gsuite.kr>
2023-02-08 15:39:10 +0900

    add: 쿠폰 적용 가능 상품 목록 추가

--- src/coupons/constants/Coupons.ts
+++ src/coupons/constants/Coupons.ts
@@ -2,10 +2,23 @@ export interface CouponType {
   name: string;
   discountRate: number;
   condition: string;
+  applicableProducts: number[];
   id: number;
 }
 
 export const Coupons: CouponType[] = [
-  { name: '미팅학개론 50% 할인 쿠폰', discountRate: 50, condition: '이용권 1장에만 사용 가능', id: 1 },
-  { name: '미팅학개론 1회 무료 이용 쿠폰', discountRate: 100, condition: '이용권 1장에만 사용 가능', id: 2 },
+  {
+    name: '미팅학개론 50% 할인 쿠폰',
+    discountRate: 50,
+    condition: '이용권 1장에만 사용 가능',
+    applicableProducts: [1],
+    id: 1,
+  },
+  {
+    name: '미팅학개론 1회 무료 이용 쿠폰',
+    discountRate: 100,
+    condition: '이용권 1장에만 사용 가능',
+    applicableProducts: [1],
+    id: 2,
+  },
 ];
