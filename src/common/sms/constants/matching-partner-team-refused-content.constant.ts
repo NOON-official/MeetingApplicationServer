9d01123733f4c97751d05e2e552330173ec6c72f
MoonGyu1 <s201801402@hufs-gsuite.kr>
2023-02-27 18:31:31 +0900

    fix: 문자 내용 수정

--- src/common/sms/constants/matching-partner-team-refused-content.constant.ts
+++ src/common/sms/constants/matching-partner-team-refused-content.constant.ts
@@ -1,12 +1,4 @@
 export const MatchingPartnerTeamRefusedContentConstant = {
   SUBJECT: `안녕하세요, [미팅학개론]입니다!`,
-  CONTENT: `기다렸을텐데 미안해 ㅜ_ㅜ
-
-  안녕하세요, [미팅학개론]입니다!
-  
-  아쉽게도 상대방이 거절을 하셨어요.
-  
-  (이건 비밀 꿀팁인데.. 자기소개서와 ID카드를 더 매력적으로 꾸며보시는건 어떨까요!?)
-  
-  지금 재매칭하러 가기. [meetingo.me]`,
+  CONTENT: `[미팅학개론]\n상대방의 수락 여부가 나왔어요. 결과를 확인해 주세요.\n\n[meetingo.me/matching]`,
 };
