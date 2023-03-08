c9689105d9cdbfe2671940299e07c37a8c62bdee
MoonGyu1 <s201801402@hufs-gsuite.kr>
2023-03-08 18:07:00 +0900

    feat: 무응답 회원 종료 3시간 전 문자 발송

---
+++ src/matchings/listeners/matching-ourteam-not-responded.listener.ts
@@ -0,0 +1,30 @@
+import { postNaverCloudSMS } from 'src/common/sms/post-navercloud-sms';
+import { TeamsService } from '../../teams/teams.service';
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import { OnEvent } from '@nestjs/event-emitter';
+import { SmsType } from 'src/common/sms/enums/sms-type.enum';
+import { ContentType } from 'src/common/sms/enums/content-type.enum';
+import { MatchingOurteamNotRespondedEvent } from '../events/matching-ourteam-not-responded.event';
+import { MatchingOurteamNotRespondedContentConstant } from 'src/common/sms/constants/matching-ourteam-not-responded-content.constant';
+
+@Injectable()
+export class MatchingOurteamNotRespondedListener {
+  constructor(
+    @Inject(forwardRef(() => TeamsService))
+    private teamsService: TeamsService,
+  ) {}
+
+  // 수락/거절 대기자 종료 3시간 전 문자 발송
+  @OnEvent('matching.ourteamNotResponded', { async: true })
+  async handleMatchingOurteamNotRespondedEvent(event: MatchingOurteamNotRespondedEvent) {
+    const team = await this.teamsService.getTeamById(event.teamId);
+
+    postNaverCloudSMS(
+      SmsType.LMS,
+      ContentType.COMM,
+      team.user.phone,
+      MatchingOurteamNotRespondedContentConstant.CONTENT,
+      MatchingOurteamNotRespondedContentConstant.SUBJECT,
+    );
+  }
+}
