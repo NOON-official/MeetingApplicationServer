7d530688e38f3bee9f2c927b90e9c1ba6ea42115
MoonGyu1 <s201801402@hufs-gsuite.kr>
2023-03-22 17:18:23 +0900

    rm: default null

--- src/database/migrations/1679302176990-MatchingRefuseReasonRefactoring.ts
+++ src/database/migrations/1679302176990-MatchingRefuseReasonRefactoring.ts
@@ -2,9 +2,7 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class MatchingRefuseReasonRefactoring1679302176990 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `ALTER TABLE matching_refuse_reason ADD COLUMN deletedAt timestamp(6) DEFAULT NULL AFTER createdAt;`,
-    );
+    await queryRunner.query(`ALTER TABLE matching_refuse_reason ADD COLUMN deletedAt timestamp(6) AFTER createdAt;`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
