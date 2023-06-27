fa59f18c4cf589c7eb0588421e1aa349310c57e1
gyuwonMoon <s201801402@hufs-gsuite.kr>
2023-06-27 15:44:51 +0900

    feat: 관리자페이지 쿠폰지급시 문자 발송

--- src/coupons/coupons.module.ts
+++ src/coupons/coupons.module.ts
@@ -4,10 +4,11 @@ import { forwardRef, Module } from '@nestjs/common';
 import { CouponsService } from './coupons.service';
 import { CouponsRepository } from './repositories/coupons.repository';
 import { UsersModule } from 'src/users/users.module';
+import { CouponCreatedListener } from './listeners/coupon-created.listener';
 
 @Module({
   imports: [TypeOrmExModule.forCustomRepository([CouponsRepository]), forwardRef(() => UsersModule)],
-  providers: [CouponsService],
+  providers: [CouponsService, CouponCreatedListener],
   controllers: [CouponsController],
   exports: [CouponsService],
 })
