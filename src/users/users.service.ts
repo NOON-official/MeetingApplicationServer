ed897dc96bb0987039581e8238ed6a267cfe1ff0
sinbox0701 <psj090953@gmail.com>
2023-08-02 15:07:42 +0900

    deleteMatchingByUserId 변경

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -27,6 +27,7 @@ import { SaveStudentCardDto } from 'src/auth/dtos/save-student-card.dto';
 import { GetTeamCardDto } from 'src/teams/dtos/get-team-card.dto';
 import { MatchingsService } from 'src/matchings/matchings.service';
 import { TingsService } from 'src/tings/tings.service';
+import { DeleteMatchingDto } from 'src/matchings/dtos/delete-matching.dto';
 
 @Injectable()
 export class UsersService {
@@ -371,11 +372,11 @@ export class UsersService {
     return this.matchingsService.getSucceededTeamCardsByUserId(userId);
   }
 
-  async deleteMatchingByUserId(userId: number, matchingIds: number[]): Promise<void> {
+  async deleteMatchingByUserId(userId: number, matchingIds: DeleteMatchingDto[]): Promise<void> {
     const user = await this.getUserById(userId);
     if (user) {
-      for (const matchingId of matchingIds) {
-        await this.matchingsService.deleteMatchingAndTeamByMatchingId(matchingId);
+      for (const matching of matchingIds) {
+        await this.matchingsService.deleteMatchingAndTeamByMatchingId(matching.matchingId);
       }
     }
   }
