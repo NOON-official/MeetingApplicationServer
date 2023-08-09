b01ccac52d34bb593f7e198d4347c6c1f3dbcb8d
gyuwonMoon <s201801402@hufs-gsuite.kr>
2023-08-09 21:52:26 +0900

    fix type string to varchar(255) of teamName

--- src/database/migrations/1686383414835-UpdateTeamEntity.ts
+++ src/database/migrations/1686383414835-UpdateTeamEntity.ts
@@ -1,33 +1,35 @@
-import { MigrationInterface, QueryRunner, TableColumn } from "typeorm"
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
 
 export class UpdateTeamEntity1686383414835 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('team_available_date');
+    await queryRunner.dropColumns('team', [
+      'prefSameUniversity',
+      'startRound',
+      'currentRound',
+      'teamAvailableDate',
+      'lastFailReason',
+      'universities',
+    ]);
+    await queryRunner.addColumns('team', [
+      new TableColumn({
+        name: 'teamAvailableDate',
+        type: 'int',
+        default: 3,
+      }),
+      new TableColumn({
+        name: 'teamName',
+        type: 'varchar(255)',
+      }),
+      new TableColumn({
+        name: 'memberCounts',
+        type: 'json',
+        isNullable: true,
+      }),
+    ]);
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable('team_available_date');
-        await queryRunner.dropColumns('team',['prefSameUniversity','startRound','currentRound','teamAvailableDate','lastFailReason','universities']);
-        await queryRunner.addColumns('team', [
-            new TableColumn({
-                name: 'teamAvailableDate',
-                type: 'int',
-                default: 3,
-            }),
-            new TableColumn({
-                name: 'teamName',
-                type: 'string',
-            }),
-            new TableColumn({
-                name: 'memberCounts',
-                type: 'json',
-                isNullable: true,
-            }),
-        ]);
-
-
-
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-       //
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    //
+  }
 }
