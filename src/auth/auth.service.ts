5294a0319643bcd2417e10a0e84e7ee8ecaf0cc5
sinbox0701 <psj090953@gmail.com>
2023-09-20 14:46:17 +0900

    Pass Up-hash

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -20,6 +20,10 @@ import { ContentType } from 'src/common/sms/enums/content-type.enum';
 import { HttpService } from '@nestjs/axios';
 import { catchError, firstValueFrom } from 'rxjs';
 import { SaveStudentCardDto } from './dtos/save-student-card.dto';
+import * as fs from 'fs';
+import { MakeUpHash } from './constants/make-up-hash';
+import { getCurrentDate, makeOrderId, makeSignatureData } from './utils/functions';
+import axios from 'axios';
 
 export class AuthService {
   constructor(
@@ -227,10 +231,67 @@ export class AuthService {
 
     if (result.nickname === null) {
       throw new NotFoundException(`Can't find user with id ${userId}`);
+    } else {
+      this.usersService.updateStudentCard(userId, saveStudentCardDto);
     }
-    else {
-      this.usersService.updateStudentCard(userId, saveStudentCardDto)
-    }
+  }
 
+  async makeUpHash(): Promise<{
+    res_cd: string;
+    res_msg: string;
+    site_cd: string;
+    ordr_idxx: string;
+    req_tx: string;
+    cert_method: string;
+    up_hash: string;
+    cert_otp_use: string;
+    web_siteid_hashYN: string;
+    web_siteid: string;
+    cert_enc_use_ext: string;
+    kcp_merchant_time: string;
+    kcp_cert_lib_ver: string;
+  }> {
+    const kcp_cert_info = fs
+      .readFileSync('./src/certificate/KCP_AUTH_AJOAD_CERT.pem', 'utf-8')
+      .toString()
+      .split('\r\n')
+      .join('');
+    const { CT_TYPE, SITE_CD, TAX_NO, WEB_SITEID, WEB_SITE_HASHYN } = MakeUpHash;
+    const ordr_idxx = makeOrderId();
+    const make_req_dt = getCurrentDate();
+    const hash_data = SITE_CD + '^' + CT_TYPE + '^' + TAX_NO + '^' + make_req_dt; //up_hash 생성 서명 데이터
+    const kcp_sign_data = makeSignatureData(hash_data); //서명 데이터(무결성 검증)
+
+    // up_hash 생성 REQ DATA
+    const req_data = {
+      site_cd: SITE_CD,
+      kcp_cert_info: kcp_cert_info,
+      ct_type: CT_TYPE,
+      ordr_idxx: ordr_idxx,
+      web_siteid: WEB_SITEID,
+      tax_no: TAX_NO,
+      make_req_dt: make_req_dt,
+      kcp_sign_data: kcp_sign_data,
+    };
+
+    const res_data = await axios.post('https://spl.kcp.co.kr/std/certpass', req_data);
+
+    console.log(res_data);
+
+    return {
+      res_cd: res_data.data.res_cd,
+      res_msg: res_data.data.res_mes,
+      site_cd: SITE_CD,
+      ordr_idxx,
+      req_tx: 'CERT',
+      cert_method: '01',
+      up_hash: res_data.data.up_hash,
+      cert_otp_use: 'Y',
+      web_siteid_hashYN: WEB_SITE_HASHYN,
+      web_siteid: WEB_SITEID,
+      cert_enc_use_ext: 'Y',
+      kcp_merchant_time: res_data.data.kcp_merchant_time,
+      kcp_cert_lib_ver: res_data.data.kcp_cert_lib_ver,
+    };
   }
 }
---
+++ src/auth/constants/make-up-hash.ts
@@ -0,0 +1,7 @@
+export const MakeUpHash = {
+  SITE_CD: 'AJOAD', //가맹정(미팅학개론) 사이트 코드
+  CT_TYPE: 'HAS', // 요청 타입
+  WEB_SITEID: 'J23090509721', //사이트 식별 코드
+  TAX_NO: '000000',
+  WEB_SITE_HASHYN: 'Y',
+};
